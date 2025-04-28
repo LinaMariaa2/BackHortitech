cf0f4140cd1c1165825e7a6dea244155196740f5
sebastianCF <luissebastianclavijof@gmail.com>
2025-04-28 11:57:45 -0500

    middleware sebastian

--- src/controllers/lecturaSensoresController.ts
+++ src/controllers/lecturaSensoresController.ts
@@ -1,3 +1,83 @@
 // Luis Sebastian
-import { Request, Response } from 'express';
-import { LecturaSensores } from '../models/lecturaSensores';
+import type { Request, Response } from 'express';
+import LecturaSensores from '../models/lecturaSensores';
+export class lecturaSensoresController {
+
+  //mostramos todas las lecturas
+  static getAll = async (req: Request, res: Response) => {
+    try {
+      const lectura = await LecturaSensores.findAll({
+        order: [['id_lectura', 'ASC']],
+      });
+      res.json(lectura); // solo se ejecuta dando repsuesta con fromato JSon
+    } catch (error) {
+      res.status(500).json({ error: "error al obtener las lecturas", details: error.message });
+    }
+
+  };
+
+
+  //mostramos lecturas con Id en URL
+
+  static getId = async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params
+      const lectura = await LecturaSensores.findByPk(id)
+      if (!lectura) {
+        const error = new Error('publicacion no encontrada')
+        res.status(404).json({ error: error.message })
+      }
+      res.json(lectura);
+    } catch (error) {
+      res.status(500).json({ error: 'error al obtener las lecturas por id', details: error.message });
+    }
+  };
+
+  //crear una nueva lectura
+
+  static crearLecturaSensores = async (req: Request, res: Response) => {
+    try {
+      const lectura = new LecturaSensores(req.body)
+      await lectura.save()
+      res.status(201).json("lectura creada correctamente")
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({ error: "Hubo un error" })
+    }
+  }
+
+  //actualizar una lectura
+  static actualizarLecturaSensores = async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      const [rowsUpdated] = await LecturaSensores.update(req.body, {
+        where: { id_lectura: id },
+      });
+      if (rowsUpdated === 0) {
+        res.status(404).json({ error: 'lectura no encontrada' });
+      }
+      res.json({ mensaje: 'lectura actualizada conrrectamente' });
+    } catch (error) {
+      console.error('error al actualizar lectura:', error);
+      res.status(500).json({
+        error: 'error al actualizar la publicacion',
+        detaller: error instanceof Error ? error.message : String(error)
+      });
+    }
+  }
+
+  static eliminarLecturaSensores = async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      const deleted = await LecturaSensores.destroy({ where: { id_lectura: id } });
+      if (deleted === 0) {
+        res.status(404).json({ error: 'lectura no encontrada' })
+        return;
+      }
+      res.json({ mesaje: 'lectura eliminada correctamente' })
+    } catch (error) {
+      res.status(500).json({ error: 'error al eliminar la lectura', details: error.message });
+    }
+  };
+
+}
--- src/routes/personaRouter.ts
+++ src/routes/personaRouter.ts
@@ -1,12 +1,36 @@
 import { Router } from 'express';
 import { personaController } from '../controllers/personaController';
-
+import { handleInputErrors } from '../middleware/validation';
+import {body, param} from 'express-validator'
+import{validatePersonaId,  validatePersonaBody} from '../middleware/personaValidator'
 const router = Router();
 
+//mostrar todas las personas
 router.get('/', personaController.getAll);
-router.get('/:id', personaController.getId);
-router.post('/', personaController.crearPersona);
-router.put('/:id', personaController.actualizarPersona);
-router.delete('/:id', personaController.eliminarPersona);
+//mostrar personas por id
+router.get('/:id', 
+    validatePersonaId,
+    personaController.getId
+);
+
+//crear una persona
+router.post('/', 
+    validatePersonaBody,
+    handleInputErrors, 
+    personaController.crearPersona);
+
+//actualizar datos de persona
+router.put('/:id', 
+    validatePersonaId,
+    validatePersonaBody, 
+    handleInputErrors,
+    personaController.actualizarPersona);
+
+//eliminar perosona
+router.delete('/:id',
+    validatePersonaBody,
+    validatePersonaId,
+    handleInputErrors,
+    personaController.eliminarPersona);
 
 export default router;
